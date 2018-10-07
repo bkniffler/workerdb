c552690e922bb2739df331c104155d0294d77365
Benjamin Kniffler <bkniffler@me.com>
2018-10-07 18:16:46 +0200

    bump

--- example/react/app.js
+++ example/react/app.js
@@ -4,7 +4,14 @@ import App from './components/app';
 
 const worker = new Worker('./worker.js');
 export default () => (
-  <WorkerDB worker={worker} loading={() => 'Is loading ...'}>
+  <WorkerDB
+    worker={worker}
+    loading={() => (
+      <div style={{ textAlign: 'center', height: 200, padding: 50 }}>
+        Is loading ...
+      </div>
+    )}
+  >
     <App />
   </WorkerDB>
 );
